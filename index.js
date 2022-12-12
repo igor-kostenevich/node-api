7f64953f57b2eafdad50b47b21eda5fc93277449
ikostenevich <kostenevich9696@gmail.com>
2022-12-12 16:16:30 +0200

    update headers

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const DB_URL = `mongodb+srv://Igor:password123321@cluster0.tib9cqk.mongodb.net/?
 const app = express()
 app.use(express.json())
 app.use('/api', router)
+
 // app.use('/api', userRouter) // If have another routes
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+  next()
+})
 
 
 
