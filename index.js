387917f23ca05dac5461ac967977fdbe63a6495b
ikostenevich <kostenevich9696@gmail.com>
2022-12-12 16:57:34 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+const cors = require('cors');
 import router from './router.js'
 
 const PORT = 5001
@@ -13,25 +14,7 @@ const app = express()
 //   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 //   next()
 // })
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors())
 
 app.use(express.json())
 app.use('/api', router)
